c078188c080b030d656b31e590e250a45808d8c0
corestate55 <manabu.hagiwara@okinawaopenlabs.org>
2025-08-17 17:26:47 +0900

    WIP: tobe data

--- lib/topology_ops/netomox_patch.rb
+++ lib/topology_ops/netomox_patch.rb
@@ -4,11 +4,11 @@ require 'netomox'
 
 module Netomox
   module Topology
+    # shutdown bridge name
+    SHUTDOWN_BRIDGE_NAME = 'Seg_empty00'
+
     # patches for Networks
     class Network
-      # shutdown bridge name
-      SHUTDOWN_BRIDGE_NAME = 'Seg_empty00'
-
       def find_link_by_destination(node_ref, tp_ref)
         destination_data = {
           'dest-node' => node_ref,
@@ -42,6 +42,66 @@ module Netomox
 
         # find empty-segment node that has no term-point (link) without shutdown-bridge
         empty_seg_nodes.find_all { |node| node.termination_points.empty? && node.name != SHUTDOWN_BRIDGE_NAME }
+      end
+
+      # @param [Netomox::Topology::Link] link Link
+      # @return [Boolean] true if link is shutdown-bridge link
+      def is_empty_bridge_link?(link)
+        link.source.node_ref == SHUTDOWN_BRIDGE_NAME || link.destination.node_ref == SHUTDOWN_BRIDGE_NAME
+      end
+    end
+
+    # patches for link
+    class Link
+      # another constructor
+      # @param [Netomox::Topology::TpRef] source Source node/tp
+      # @param [Netomox::Topology::TpRef] destination Destination node/tp
+      # @param [String] layer Layer name
+      # @return [Netomox::Topology::Link]
+      def self.from_tpref(source, destination, layer)
+        data = {
+          'link-id' => "#{source.node_ref},#{source.tp_ref},#{destination.node_ref},#{destination.tp_ref}",
+          'source' => source.to_data('source'),
+          'destination' => destination.to_data('dest'),
+        }
+        new(data, layer)
+      end
+
+      # @param [String] node_ref Node name
+      # @return [Netomox::Topology::TpRef, nil] nil if not found
+      def find_endpoint_by_node(node_ref)
+        return @source if @source.node_ref == node_ref
+        return @destination if @destination.node_ref == node_ref
+        nil
+      end
+
+      # @return [Netomox::Topology::TpRef, nil] nil if not found
+      def find_shutdown_endpoint
+        find_endpoint_by_node(SHUTDOWN_BRIDGE_NAME)
+      end
+
+      # @param [Netomox::Topology::TpRef] endpoint Link endpoint
+      # @return [Netomox::Topology::TpRef, nil] counterpart (facing) endpoint, nil if not found
+      def find_counterpart_endpoint(endpoint)
+        return @source unless @source == endpoint
+        return @destination unless @destination == endpoint
+        nil
+      end
+    end
+
+    # patches for TpRef
+    class TpRef
+      # another constructor
+      # @param [String] node Node name
+      # @param [String] term_point Term-point name
+      # @param [String] layer Layer name
+      # @return [Netomox::Topology::TpRef]
+      def self.from_name(node, term_point, layer)
+        data = {
+          'source-node' => node,
+          'source-tp' => term_point,
+        }
+        new(data, layer)
       end
     end
   end
--- lib/topology_ops/topology_ops_commander.rb
+++ lib/topology_ops/topology_ops_commander.rb
@@ -30,8 +30,8 @@ module ModelConductor
 
       current_resource = {
         'links' => [
-          orig_l3nw.find_all_links_connect(@arg_link.source.to_tpref),
-          orig_l3nw.find_all_links_connect(@arg_link.destination.to_tpref)
+          orig_l3nw.find_all_links_connect(@arg_link.source.to_tpref), # link0
+          orig_l3nw.find_all_links_connect(@arg_link.destination.to_tpref) # link1
         ],
         'empty_bridges' => orig_l3nw.find_all_empty_bridges
       }
@@ -39,19 +39,76 @@ module ModelConductor
       {
         'operation' => operation,
         'current_resource' => current_resource,
-        'tobe' => operate_tobe(orig_l3nw, operation, current_resource)
+        'tobe_resource' => operate_tobe(orig_l3nw, operation, current_resource)
       }
     end
 
     private
+
+    # @param [Array<Netomox::Topology::Link>] link_pair Link pair (current)
+    # @param [Netomox::Topology::Node] empty_bridge Empty bridge
+    # @return [Hash]
+    def move_shutdown_bridge_link(link_pair, empty_bridge)
+      # alias: pair = [a->b, b->a], only use first entry
+      link0 = link_pair[0]
+      # answers
+      # cli_commands = [] # TBA
+      remove_links = []
+      append_links = []
+
+      shut_ep = link0.find_shutdown_endpoint
+      keep_ep = link0.find_counterpart_endpoint(shut_ep)
+      new_ep = Netomox::Topology::TpRef.from_name(empty_bridge.name, shut_ep.tp_ref, 'layer3')
+
+      remove_links.push(link_pair)
+      append_links.push(Netomox::Topology::Link.from_tpref(keep_ep, new_ep, 'layer3'))
+      append_links.push(Netomox::Topology::Link.from_tpref(new_ep, keep_ep, 'layer3'))
+
+      {
+        'remove_links' => remove_links,
+        'append_links' => append_links,
+        'command_list' => ['TBA'] # TBA
+      }
+    end
+
+    # @param [Array<Hash>] ops_answers Operation answer list
+    # @param [Array<Netomox::Topology::Node>] empty_bridges Empty bridge list (tobe/after)
+    # @return [Hash]
+    def merge_operations(ops_answers, empty_bridges)
+      {
+        'remove_links' => ops_answers.map { |op| op['remove_links'] }.flatten(1),
+        'append_links' => ops_answers.map { |op| op['append_links'] },
+        'command_list' => ops_answers.map { |op| op['command_list'] },
+        'empty_bridge' => empty_bridges
+      }
+    end
 
     # @param [Netomox::Topology::Network] layer3_nw Layer3 network
     # @param [Hash] operation Operation data
     # @param [Hash] current_resource Current resource data
     # @return [Hash]
     def operate_tobe(layer3_nw, operation, current_resource)
-      # TDOO
-      {}
+      # alias
+      link0 = current_resource['links'][0] # pair of 00, 01: a->b, b->a pair
+      link00 = link0[0]
+      link1 = current_resource['links'][1] # pair of 10, 11: c->d, d->c pair
+      link10 = link1[0]
+
+      if layer3_nw.is_empty_bridge_link?(link00) || layer3_nw.is_empty_bridge_link?(link10)
+        # pattern [1][2]
+        tobe_empty_bridges = current_resource['empty_bridges'].dup # keep current list
+        ebr = tobe_empty_bridges.shift
+        raise StandardError, "Empty bridge:#{ebr.name} is not found in layer3 network" if ebr.nil?
+
+        ans = []
+        ans.push(move_shutdown_bridge_link(link0, ebr)) if layer3_nw.is_empty_bridge_link?(link00)
+        ans.push(move_shutdown_bridge_link(link1, ebr)) if layer3_nw.is_empty_bridge_link?(link10)
+        merge_operations(ans, tobe_empty_bridges)
+      else
+        # pattern [3]
+        warn `Cannot handle pattern [3], Ignored (currently)`
+        {}
+      end
     end
   end
 end
