736b7ffe10bcd0667ed840e2fd8612557ea541a4
corestate55 <manabu.hagiwara@okinawaopenlabs.org>
2025-08-18 17:05:10 +0900

    simplify yard comments

--- lib/topology_ops/netomox_patch.rb
+++ lib/topology_ops/netomox_patch.rb
@@ -18,8 +18,8 @@ module Netomox
         @links.find { |link| link.destination == destination_ref }
       end
 
-      # @param [Netomox::Topology::TpRef] endpoint Link endpoint (source or destination)
-      # @return [Array<Netomox::Topology::Link>]
+      # @param [TpRef] endpoint Link endpoint (source or destination)
+      # @return [Array<Link>]
       def find_all_links_connect(endpoint)
         [
           find_link_by_source(endpoint.node_ref, endpoint.tp_ref),
@@ -28,7 +28,7 @@ module Netomox
       end
 
       # @param [String] node_type Node type
-      # @return [Array<Netomox::Topology::Node>]
+      # @return [Array<Node>]
       def find_all_nodes_by_type(node_type)
         @nodes.find_all do |node|
           node.attribute.node_type == node_type
@@ -36,14 +36,14 @@ module Netomox
       end
 
       # @param [String] flag Flag
-      # @return [Array<Netomox::Topology::Node>]
+      # @return [Array<Node>]
       def find_all_node_by_flag(flag)
         @nodes.find_all do |node|
           node.attribute.flags.include?(flag)
         end
       end
 
-      # @return [Array<Netomox::Topology::Node>]
+      # @return [Array<Node>]
       def find_all_empty_bridges
         empty_seg_nodes = find_all_node_by_flag('empty-segment')
         return [] if empty_seg_nodes.nil?
@@ -52,7 +52,7 @@ module Netomox
         empty_seg_nodes.find_all { |node| node.termination_points.empty? && node.name != SHUTDOWN_BRIDGE_NAME }
       end
 
-      # @param [Netomox::Topology::Link] link Link
+      # @param [Link] link Link
       # @return [Boolean] true if link is shutdown-bridge link
       def empty_bridge_link?(link)
         link.source.node_ref == SHUTDOWN_BRIDGE_NAME || link.destination.node_ref == SHUTDOWN_BRIDGE_NAME
@@ -62,10 +62,10 @@ module Netomox
     # patches for link
     class Link
       # another constructor
-      # @param [Netomox::Topology::TpRef] source Source node/tp
-      # @param [Netomox::Topology::TpRef] destination Destination node/tp
+      # @param [TpRef] source Source node/tp
+      # @param [TpRef] destination Destination node/tp
       # @param [String] layer Layer name
-      # @return [Netomox::Topology::Link]
+      # @return [Link]
       def self.from_tpref(source, destination, layer)
         data = {
           'link-id' => "#{source.node_ref},#{source.tp_ref},#{destination.node_ref},#{destination.tp_ref}",
@@ -76,7 +76,7 @@ module Netomox
       end
 
       # @param [Hash] arg_link Link data in command argument
-      # @return [Netomox::Topology::Link]
+      # @return [Link]
       def self.from_arg_link(arg_link)
         source = TpRef.from_name(arg_link['source']['node'], arg_link['source']['tp'], 'layer3')
         destination = TpRef.from_name(arg_link['destination']['node'], arg_link['destination']['tp'], 'layer3')
@@ -84,7 +84,7 @@ module Netomox
       end
 
       # @param [String] node_ref Node name
-      # @return [Netomox::Topology::TpRef, nil] nil if not found
+      # @return [TpRef, nil] nil if not found
       def find_endpoint_by_node(node_ref)
         return @source if @source.node_ref == node_ref
         return @destination if @destination.node_ref == node_ref
@@ -92,13 +92,13 @@ module Netomox
         nil
       end
 
-      # @return [Netomox::Topology::TpRef, nil] nil if not found
+      # @return [TpRef, nil] nil if not found
       def find_shutdown_endpoint
         find_endpoint_by_node(SHUTDOWN_BRIDGE_NAME)
       end
 
-      # @param [Netomox::Topology::TpRef] endpoint Link endpoint
-      # @return [Netomox::Topology::TpRef, nil] counterpart (facing) endpoint, nil if not found
+      # @param [TpRef] endpoint Link endpoint
+      # @return [TpRef, nil] counterpart (facing) endpoint, nil if not found
       def find_counterpart_endpoint(endpoint)
         return @source unless @source == endpoint
         return @destination unless @destination == endpoint
@@ -113,7 +113,7 @@ module Netomox
       # @param [String] node Node name
       # @param [String] term_point Term-point name
       # @param [String] layer Layer name
-      # @return [Netomox::Topology::TpRef]
+      # @return [TpRef]
       def self.from_name(node, term_point, layer)
         data = {
           'source-node' => node,
