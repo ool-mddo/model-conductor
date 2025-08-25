1d8fed13f94fc1468ed32936c76814ec6eec8d55
corestate55 <manabu.hagiwara@okinawaopenlabs.org>
2025-08-25 18:12:54 +0900

    refactoring/rename empty to preallocated

--- lib/api/conduct/network/snapshot/splice_topology.rb
+++ lib/api/conduct/network/snapshot/splice_topology.rb
@@ -2,7 +2,7 @@
 
 require 'grape'
 require 'lib/splice_topology/topology_splicer'
-require 'lib/splice_topology/layer3_empty_resource_splicer'
+require 'lib/splice_topology/layer3_preallocated_resource_splicer'
 
 module ModelConductor
   module ApiRoute
@@ -11,9 +11,9 @@ module ModelConductor
       desc 'Post external bgp topology data and splice it to (internal) topology data'
       params do
         optional :ext_topology_data, type: Hash, desc: 'External topology data to splice'
-        optional :l3_empty_resources, type: Hash, desc: 'Layer3 empty resources data to splice'
+        optional :l3_preallocated_resources, type: Hash, desc: 'Layer3 preallocated (empty) resources data to splice'
         optional :overwrite, type: Boolean, desc: 'Overwrite to topology data', default: true
-        at_least_one_of :ext_topology_data, :l3_empty_resources
+        at_least_one_of :ext_topology_data, :l3_preallocated_resources
       end
       post 'splice_topology' do
         network, snapshot, overwrite = %i[network snapshot overwrite].map { |key| params[key] }
@@ -27,10 +27,10 @@ module ModelConductor
           spliced_topology = splicer.to_data
         end
 
-        if params.key?(:l3_empty_resources)
-          l3e_topology = params[:l3_empty_resources]
+        if params.key?(:l3_preallocated_resources)
+          l3p_topology = params[:l3_preallocated_resources]
           topology = spliced_topology || rest_api.fetch_topology_data(network, snapshot)
-          splicer = Layer3EmptyResourceSplicer.new(topology, l3e_topology)
+          splicer = Layer3PreallocatedResourceSplicer.new(topology, l3p_topology)
           splicer.splice!
           spliced_topology = splicer.to_data
         end
--- lib/splice_topology/layer3_preallocated_resource_splicer.rb
+++ lib/splice_topology/layer3_preallocated_resource_splicer.rb
@@ -3,10 +3,10 @@
 require 'netomox'
 
 module ModelConductor
-  # Splice layer3 empty resources into specified network/snapshot topology
-  class Layer3EmptyResourceSplicer
+  # Splice layer3 preallocated (empty) resources into specified network/snapshot topology
+  class Layer3PreallocatedResourceSplicer
     # @param [Hash] topology_data Topology data to splice
-    # @param [Hash] l3e_topology_data Topology data of layer3 empty resources (RFC8345 Hash)
+    # @param [Hash] l3e_topology_data Topology data of layer3 preallocated resources (RFC8345 Hash)
     def initialize(topology_data, l3e_topology_data)
       # convert RFC8345 Hash to Netomox::Topology::Networks
       @topology = instantiate_topology_data(topology_data)
@@ -33,12 +33,12 @@ module ModelConductor
     end
 
     # @param [Netomox::PseudoDSL::PNode] layer3_target_node Target l3 node
-    # @param [Netomox::PseudoDSL::PNode] layer3e_node Empty L3 node (duplicated)
-    # @param [Netomox::PseudoDSL::PTermPoint] layer3e_tp Empty L3 tp (in layer3e_node)
-    def add_empty_tp_for_dup_node(layer3_target_node, layer3e_node, layer3e_tp)
+    # @param [Netomox::PseudoDSL::PNode] layer3e_node preallocated (empty) L3 node (duplicated)
+    # @param [Netomox::PseudoDSL::PTermPoint] layer3e_tp preallocated L3 tp (in layer3e_node)
+    def add_preallocated_tp_for_dup_node(layer3_target_node, layer3e_node, layer3e_tp)
       layer3_dup_tp = layer3_target_node.find_tp_by_name(layer3e_tp.name)
       unless layer3_dup_tp.nil?
-        raise StandardError, "Found duplicate empty resource: #{layer3e_node.name}[#{layer3e_tp.name}]"
+        raise StandardError, "Found duplicate preallocated resource: #{layer3e_node.name}[#{layer3e_tp.name}]"
       end
 
       # not found = new tp, append whole term-point
@@ -46,9 +46,9 @@ module ModelConductor
     end
 
     # @param [Netomox::PseudoDSL::PNetwork] layer3_nw Target layer3 network
-    # @param [Netomox::PseudoDSL::PNetwork] layer3e_nw layer3 network of empty resources
+    # @param [Netomox::PseudoDSL::PNetwork] layer3e_nw layer3 network of preallocated (empty) resources
     # @return [void]
-    def add_empty_node(layer3_nw, layer3e_nw)
+    def add_preallocated_node(layer3_nw, layer3e_nw)
       layer3e_nw.nodes.each do |layer3e_node|
         layer3_dup_node = layer3_nw.find_node_by_name(layer3e_node.name)
         if layer3_dup_node.nil?
@@ -57,29 +57,29 @@ module ModelConductor
         else
           # found duplicated node, but interface?
           layer3e_node.termination_points.each do |layer3e_tp|
-            add_empty_tp_for_dup_node(layer3_dup_node, layer3e_node, layer3e_tp)
+            add_preallocated_tp_for_dup_node(layer3_dup_node, layer3e_node, layer3e_tp)
           end
         end
       end
     end
 
     # @param [Netomox::PseudoDSL::PNetwork] layer3_nw Target layer3 network
-    # @param [Netomox::PseudoDSL::PNetwork] layer3e_nw layer3 network of empty resources
+    # @param [Netomox::PseudoDSL::PNetwork] layer3e_nw layer3 network of preallocated (empty) resources
     # @return [void]
-    def add_links_between_empty_resource(layer3_nw, layer3e_nw)
+    def add_links_between_preallocated_resource(layer3_nw, layer3e_nw)
       layer3e_nw.links.each do |layer3e_link|
         layer3_nw.links.push(layer3e_link)
       end
     end
 
-    # raise [StandardError] found duplicated object in layer3 between target topology and empty-resource
+    # raise [StandardError] found duplicated object in layer3 between target topology and preallocated-resource
     # @return [void]
     def splice_layer3!
       layer3_nw = @topology.find_network('layer3')
       layer3e_nw = @l3e_topology.find_network('layer3')
 
-      add_empty_node(layer3_nw, layer3e_nw)
-      add_links_between_empty_resource(layer3_nw, layer3e_nw)
+      add_preallocated_node(layer3_nw, layer3e_nw)
+      add_links_between_preallocated_resource(layer3_nw, layer3e_nw)
     end
   end
 end
